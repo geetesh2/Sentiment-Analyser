ed175909dc7ab82643605c80ee1075aa6d6ccff9
Geetesh Pandey <geeteshpandey2@gmail.com>
2023-03-24 10:48:57 +0530

    Everything done

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, request
 import flask
 import json
 app = Flask(__name__)
+data = {}
 
 @app.route('/')
 def hello_world():
-   return render_template('index.html')
+   return render_template('index.html',data=data,dis = "hidi")
 
 
 
@@ -100,7 +101,7 @@ def valid():
       # Sample sentiment analysis results
 
       data = {'Emotion' : 'Count',"positive": pos_num, "negative": neg_num, "neutral": neu_num}
-      return render_template('index.html',data=data)
+   return render_template('index.html',data=data,dis = "hid")
 
 if __name__ == '__main__':
     
